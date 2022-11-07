426aebbbe9acab87234a22f80b39ceccf06e1d00
thanhhuy29111 <73162402+thanhhuy29111@users.noreply.github.com>
2022-11-07 19:27:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Amyyy ơi!',
-    introDesc: `Tớ có câu hỏi này muốn hỏi em bé
+    introTitle: '... ơi!',
+    introDesc: `Tớ có câu hỏi này muốn hỏi
     Trả lời thật lòng nhaaa :>>`,
     btnIntro: 'Okieee',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Em bé có yêu tớ honggg ',
+    desc: '... có yêu tớ honggg ',
     btnYes: 'À thì có yêu đấyyy <33',
     btnNo: 'Mơ àaa :>>>',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em bé lại yêu tớ <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao ... lại yêu tớ <3',
     btnReply: 'Gửi cho tớ <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu em bé nhiều nhiều 😘😘',
+    mess: 'Tớ biết mà 🥰. Yêu ... nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h tớ qua đón công chúa nhaaaaa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/kenken.609' //link mess của các bạn. VD: https://m.me/nam.nodemy
